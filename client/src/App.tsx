2617768aeb3d1fa8eedb5014c655a55bd622375d
simon <simonaertsbecode@gmail.com>
2022-06-10 10:30:55 +0200

    implemented redux

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+
+//**Redux import */
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
+//**Componenent import */
 import Routes from './routes/Routes';
 import Navigation from './routes/Navigation';
 
 function App() {
    return (
       <BrowserRouter>
-         <Navigation />
-         <section className='container'>
-            <Routes />
-         </section>
+         <Provider store={store}>
+            <Navigation />
+            <section className='container'>
+               <Routes />
+            </section>
+         </Provider>
       </BrowserRouter>
    );
 }
